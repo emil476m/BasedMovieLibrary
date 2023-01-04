e1fc0f8668a15b3ea4cb4fb35008e15feebc3f3f
emil476m <emilwiltmann.dk@gmail.com>
2023-01-04 10:39:46 +0100

    view now opens

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/Views/MainView.fxml"));
+        primaryStage.setTitle("Based Movie Collection");
+        //primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setScene(new Scene(root));
+
+
+        //Add fixed height and width here
+
+
+        primaryStage.show();
     }
 }
