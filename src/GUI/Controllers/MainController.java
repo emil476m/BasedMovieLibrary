db29577337a8e247778008e3ba76c889ad8e8550
andreas14101 <andreasshansen@live.dk>
2023-01-04 13:02:07 +0100

    Play movie

--- src/GUI/Controllers/MainController.java
+++ src/GUI/Controllers/MainController.java
@@ -1,5 +1,7 @@
 package GUI.Controllers;
 
+import BE.Movie;
+import GUI.Util.ExceptionHandler;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -7,7 +9,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 
+import java.awt.*;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -33,6 +39,9 @@ public class MainController extends BaseController implements Initializable {
     @FXML
     private Button btnsearch;
 
+    private ExceptionHandler exceptionHandler;
+    private Movie selectedMovie;
+    private File directory;
     @FXML
     private void handleAddMovie(ActionEvent actionEvent) {
     }
@@ -44,6 +53,32 @@ public class MainController extends BaseController implements Initializable {
     }
     @FXML
     private void handleSearch(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void playMovie(MouseEvent mouseEvent)
+    {
+        if(mouseEvent.getButton() == MouseButton.PRIMARY)
+        {
+            openMediaPlayer();
+        }
+    }
+
+    private void openMediaPlayer()
+    {
+       selectedMovie = (Movie) tbvMovies.getSelectionModel().getSelectedItem()
+        directory = new File(selectedMovie.getFilePath());
+        Desktop desktop = Desktop.getDesktop();
+        if(desktop.isSupported(Desktop.Action.OPEN))
+        {
+            try {
+                desktop.open(directory.getAbsoluteFile());
+            }
+            catch (Exception ex)
+            {
+                exceptionHandler.displayError(ex);
+            }
+        }
     }
 
     @Override
